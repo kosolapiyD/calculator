ecb7bbec6e6c34a7632ad545439b471078ca317c
Medved Dima <dimam@assuta.co.il>
2023-04-28 13:57:00 +0300

    calculation using eval added

--- src/App.tsx
+++ src/App.tsx
@@ -6,31 +6,99 @@ import { ThemeContext } from './context/ThemeContext';
 
 const App = () => {
   const { theme } = useContext(ThemeContext);
+
+  const [calculation, setCalculation] = useState('');
+  const [result, setResult] = useState('');
+
+  const operators = ['/', 'x', '+', '-', '.'];
+
+  const updateCalculation = (value: string) => {
+    if (
+      (operators.includes(value) && calculation === '') ||
+      (operators.includes(value) && operators.includes(calculation.slice(-1)))
+    )
+      return;
+
+    setCalculation(calculation + value);
+
+    if (!operators.includes(value)) {
+      setResult(eval(calculation + value).toString());
+    }
+  };
+
+  const calculate = () => {
+    setCalculation(eval(calculation).toString());
+  };
+
+  const deleteLast = () => {
+    if (calculation === '') return;
+    setCalculation(calculation.slice(0, -1));
+  };
+
+  const reset = () => {
+    setCalculation('');
+    setResult('');
+  };
+
+  const handleBtnClick = (value: string) => {
+    switch (value) {
+      case '=':
+        calculate();
+        break;
+      case 'RESET':
+        reset();
+        break;
+      case 'DEL':
+        deleteLast();
+        break;
+      default:
+        updateCalculation(value);
+        break;
+    }
+  };
+
+  const buttonsSigns: string[] = [
+    '7',
+    '8',
+    '9',
+    'DEL',
+    '4',
+    '5',
+    '6',
+    '+',
+    '1',
+    '2',
+    '3',
+    '-',
+    '.',
+    '0',
+    '/',
+    'x',
+    'RESET',
+    '=',
+  ];
+  const buttons: JSX.Element[] = buttonsSigns.map((sign: string) => {
+    return (
+      <button
+        key={sign}
+        onClick={() => handleBtnClick(sign)}
+        className={`${
+          sign === 'RESET' ? 'span-two' : '' || sign === '=' ? 'span-two' : ''
+        }`}
+      >
+        {sign}
+      </button>
+    );
+  });
+
   return (
     <div className={`calc-wrapper ${theme}`}>
       <div className='container'>
         <CalcHeader />
-        <div className='calc-screen theme-text'>399,982</div>
-        <div className='calc-body'>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>DEL</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>+</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>-</button>
-          <button>.</button>
-          <button>0</button>
-          <button>/</button>
-          <button>x</button>
-          <button className='span-two'>RESET</button>
-          <button className='span-two'>=</button>
+        <div className='calc-screen theme-text'>
+          {result ? <span>({result})</span> : ''}&nbsp;{calculation || '0'}
         </div>
+        <div className='calc-body'>{buttons}</div>
       </div>
     </div>
   );
